1973ae8a034ca48363058ce383e1ae37e92e8012
Bryan Machado <bryanmachado1703@icloud.com>
2025-02-16 15:34:54 -0300

    Update 6 - Creating the user interaction menu.

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+import java.util.Scanner;
+
+    public class Principal {
+
+        private Scanner leitura = new Scanner(System.in);
+
+        private ConsumoApi consumo = new ConsumoApi();
+
+        private final String ENDERECO = "https://omdbapi.com/?t=";
+        private final String API_KEY = "apikey=6585022c";
+
+        public void exibeMenu(){
+            System.out.println("Digite o nome da série para busca:");
+            var nomeSerie = leitura.nextLine();
+            var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ","+") + API_KEY);
+
+        }
+
+
+    }
